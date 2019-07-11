b832391d0e8f7462ac50ea59a67ab46bbe166dc0
zakahs <zakash@gmail.com>
2019-07-11 03:18:54 -0700

    port update

--- server/app.js
+++ server/app.js
@@ -43,4 +43,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'))
 });
 
-app.listen(3000, () => console.log("Listening on port 3000..."));
+app.listen(process.env.PORT || 8080, () => console.log("Listening on port" + process.env.PORT));
